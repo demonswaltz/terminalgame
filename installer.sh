bb942360ff3bfdbf0658ed83cdab0f35cfc25f5f
demonswaltz <jessica.l.duffy@gmail.com>
2017-09-22 12:33:06 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -18,7 +18,7 @@ apt-get install -y jq whiptail
 # Install Games
 apt-get -y install crawl dopewars empire greed mancala moon-buggy nethack-console ninvaders nsnake slashem tint
 
-wget https://raw.githubusercontent.com/AllGray/AllGray_Private/master/terminalgame/launcher.sh
+wget https://raw.githubusercontent.com/demonswaltz/terminalgame/master/launcher.sh
 chmod +x launcher.sh
 
 #Done!
